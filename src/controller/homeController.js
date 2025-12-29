f231370cacabe9215ed3b33f012ed62faa90a41b
trung kim <trungkim8694@gmail.com>
2025-12-29 21:33:18 +0700

    video 15

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -1,3 +1,5 @@
+import userService from "../service/userService";
+
 const handleHelloWord = (req, res) => {
   return res.render("home.ejs");
 };
@@ -5,8 +7,20 @@ const handleUserPage = (req, res) => {
   //model => get data from database
   return res.render("user.ejs");
 };
+const handleCreateNewUser = (req, res) => {
+  let email = req.body.email;
+  let password = req.body.password;
+  let username = req.body.username;
+
+  //   userService.createNewUser(email, password, username);
+
+  userService.getUserList();
+
+  return res.send("handleCreateNewUser");
+};
 
 module.exports = {
   handleHelloWord,
   handleUserPage,
+  handleCreateNewUser,
 };
--- src/server.js
+++ src/server.js
@@ -2,12 +2,19 @@ import express from "express";
 import configViewEngine from "./configs/viewEngine";
 import initWebRoutes from "./routes/web";
 require("dotenv").config();
-
 const app = express();
+
+import bodyParser from "body-parser";
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 const PORT = process.env.PORT || 8080;
 
 //config view engine
 configViewEngine(app);
+
+//config body-parser
 
 // init web routes
 initWebRoutes(app);
