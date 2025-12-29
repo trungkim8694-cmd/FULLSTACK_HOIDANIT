3d9427d9c831e907e5714f9bbabe0d885dc89fc9
trung kim <trungkim8694@gmail.com>
2025-12-29 15:36:02 +0700

    first commit

---
+++ src/configs/viewEngine.js
@@ -0,0 +1,14 @@
+import express from "express";
+
+/**
+ *
+ * @param {*} app - express app
+ */
+
+const configViewEngine = (app) => {
+  app.use(express.static("./src/public"));
+  app.set("view engine", "ejs"); // định nghĩa công cụ view
+  app.set("view", "./src/views"); // file path view Engine
+};
+
+export default configViewEngine;
