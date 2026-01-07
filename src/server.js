a99c0a0d278fdfc1e511daf8618d71b865ce0f10
trung kim <trungkim8694@gmail.com>
2026-01-07 13:27:20 +0700

    video 21

--- src/server.js
+++ src/server.js
@@ -3,13 +3,16 @@ import configViewEngine from "./config/viewEngine";
 import initWebRoutes from "./routes/web";
 require("dotenv").config();
 const app = express();
-
 import bodyParser from "body-parser";
+import connection from "./config/connectDB";
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const PORT = process.env.PORT || 8080;
+
+//test connection DB
+connection();
 
 //config view engine
 configViewEngine(app);
